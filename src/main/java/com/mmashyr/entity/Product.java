dbb8d1a08cb88d57669fb948fe80100679fdb26b
mmashyr <mashyr.mark@gmail.com>
2017-01-19 18:55:38 +0200

    equals and hashCode methods override

--- src/main/java/com/mmashyr/entity/Product.java
+++ src/main/java/com/mmashyr/entity/Product.java
@@ -64,4 +64,24 @@ public class Product extends BasicEntity {
     public void setCategories(List<Category> categories) {
         this.categories = categories;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!id.equals(product.id)) return false;
+        if (!name.equals(product.name)) return false;
+        return price.equals(product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + price.hashCode();
+        return result;
+    }
 }
