84bd24f638d2ecf68a7b418d13157768e26dd556
mmashyr <mashyr.mark@gmail.com>
2017-01-19 18:58:52 +0200

    method for showing product to customer implemented

--- src/main/java/com/mmashyr/controller/customer/MainPageController.java
+++ src/main/java/com/mmashyr/controller/customer/MainPageController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -53,6 +54,11 @@ public class MainPageController {
         }
         model.addAttribute("productsToShow", productsToShow);
         return CUSTOMER_PAGES + "main";
+    }
 
+    @RequestMapping(value = "/product/{productId}")
+    public String showProduct(Model model, @PathVariable Long productId) {
+        model.addAttribute("product", productService.findOne(productId));
+        return CUSTOMER_PAGES + "product";
     }
 }
