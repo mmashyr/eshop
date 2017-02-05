2d79ff45c9e4ffa97241c99ef407b7073bb36bf0
mmashyr <mashyr.mark@gmail.com>
2017-02-05 15:05:45 +0200

    Refactored in order to work with new security logic

--- src/main/java/com/mmashyr/controller/customer/ShoppingCartController.java
+++ src/main/java/com/mmashyr/controller/customer/ShoppingCartController.java
@@ -1,9 +1,16 @@
 package com.mmashyr.controller.customer;
 
+import com.mmashyr.entity.Account;
+import com.mmashyr.entity.Booking;
 import com.mmashyr.entity.Product;
+import com.mmashyr.entity.enums.OrderStatus;
+import com.mmashyr.repository.AccountRepository;
+import com.mmashyr.repository.BookingRepository;
 import com.mmashyr.service.ProductService;
 import com.mmashyr.utils.ShoppingCart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +28,10 @@ public class ShoppingCartController {
     private final static String CART_PAGE = "customerpages/cart";
     @Autowired
     ShoppingCart shoppingCart;
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    BookingRepository bookingRepository;
     private ProductService productService;
 
     @Autowired
@@ -35,14 +46,30 @@ public class ShoppingCartController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String showShoppingCart() {
-        System.out.println("!!!!!!!!!!!!!! " + shoppingCart.getSalePositions().entrySet().size());
         return CART_PAGE;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public String addToShoppingCart(@RequestParam("productId") Long productId, @RequestParam("amount") String amount) {
         Product product = productService.findOne(productId);
-                shoppingCart.addProduct(product, Integer.parseInt(amount));
-                return "redirect:/cart";
+        shoppingCart.addProduct(product, Integer.parseInt(amount));
+        return "redirect:/cart";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveAsBooking() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String customer = authentication.getName();
+        Account account = accountRepository.findAccountByUsername(customer);
+
+        Booking booking = new Booking();
+        booking.setOrderStatus(OrderStatus.NEW);
+        booking.setProductsInBooking(shoppingCart.getSalePositions());
+        booking.setAccount(account);
+
+        bookingRepository.save(booking);
+
+        shoppingCart.removeAll();
+        return "redirect:/cart"; //TODO add flash attributes about successful purchase
     }
 }
