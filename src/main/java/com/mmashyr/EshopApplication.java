9b99ea5a632c78100f965e339ed7a8958318851c
Mashyr Mark <mashyr.mark@gmail.com>
2017-02-06 16:46:54 +0200

    Jsr310JpaConverters.class added to an EntityScan in order to work with java.time. package in a proper way

--- src/main/java/com/mmashyr/EshopApplication.java
+++ src/main/java/com/mmashyr/EshopApplication.java
@@ -4,8 +4,11 @@ import com.mmashyr.config.JpaConfig;
 import com.mmashyr.config.MvcConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = Jsr310JpaConverters.class)
 public class EshopApplication {
 
     public static void main(String[] args) {
