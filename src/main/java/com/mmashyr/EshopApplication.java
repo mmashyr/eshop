8dab0d03be7ae34d13bf39b1cab5083821321f99
Mashyr Mark <mashyr.mark@gmail.com>
2017-02-20 17:28:44 +0200

    @EnableAutoConfiguration added

--- src/main/java/com/mmashyr/EshopApplication.java
+++ src/main/java/com/mmashyr/EshopApplication.java
@@ -3,11 +3,13 @@ package com.mmashyr;
 import com.mmashyr.config.JpaConfig;
 import com.mmashyr.config.MvcConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EntityScan(basePackageClasses = Jsr310JpaConverters.class)
 public class EshopApplication {
 
